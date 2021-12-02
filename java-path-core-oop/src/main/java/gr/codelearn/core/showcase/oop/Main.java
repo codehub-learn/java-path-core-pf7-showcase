21922b3133a5a93547c03f67113f4f457ad6fa95
Giannis Klian <klian12@yahoo.com>
2021-12-02 20:10:19 +0200

    Add logger configuration

--- java-path-core-oop/src/main/java/gr/codelearn/core/showcase/oop/Main.java
+++ java-path-core-oop/src/main/java/gr/codelearn/core/showcase/oop/Main.java
@@ -2,20 +2,23 @@ package gr.codelearn.core.showcase.oop;
 
 import gr.codelearn.core.showcase.oop.model.Smartphone;
 import gr.codelearn.core.showcase.oop.model.Smartwatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 
 public class Main {
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 	public static void main(String[] args) {
 		Smartphone smartphone1 = new Smartphone();
 		smartphone1.setName("RealYou");
 		smartphone1.setPrice(BigDecimal.valueOf(259));
 		smartphone1.setTypeOfScreen("OLED");
-		System.out.println("The type of screen for " + smartphone1.getName() + " is " + smartphone1.getTypeOfScreen());
-		System.out.println(smartphone1);
+		logger.info("The type of screen for {} is {}",smartphone1.getName(), smartphone1.getTypeOfScreen());
+		logger.info("{}", smartphone1);
 
 		Smartwatch smartwatch1 = new Smartwatch("GSM-123","Garmon",BigDecimal.valueOf(500),"420x350","AMOLED",true,
 												"Plastic");
-		System.out.println(smartwatch1);
+		logger.info("{}", smartwatch1);
 	}
 }
